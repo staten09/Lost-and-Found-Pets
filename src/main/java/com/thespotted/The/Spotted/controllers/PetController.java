7fcc371a4b44a94484e1502f216653b6ecb03df9
Joe Bao <toj7278@yahoo.com>
2022-09-26 19:56:19 -0500

    Fixed CORS issue

--- src/main/java/com/thespotted/The/Spotted/controllers/PetController.java
+++ src/main/java/com/thespotted/The/Spotted/controllers/PetController.java
@@ -2,19 +2,35 @@ package com.thespotted.The.Spotted.controllers;
 
 import com.thespotted.The.Spotted.models.Pet;
 import com.thespotted.The.Spotted.service.PetService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/pet")
 public class PetController {
     private PetService petService;
 
     public PetController(PetService petService) {
         this.petService = petService;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
     @GetMapping("/all")
@@ -29,6 +45,7 @@ public class PetController {
         return new ResponseEntity<>(pet, HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/add")
     public ResponseEntity<Pet> addPet(@RequestBody Pet pet){
         Pet newPet = petService.addPet(pet);
